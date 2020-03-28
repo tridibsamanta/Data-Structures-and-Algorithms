4b727db25ad1f6458aa9526a89d1a183be62b4c8
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-03-28 22:32:26 +0530

    Preorder traversal of a binary tree

---
+++ DS/BinaryTree_PreorderTraversal.cpp
@@ -0,0 +1,81 @@
+/*
+Preorder Traversal of a Binary Tree
+
+Example -
+              F
+            /   \
+           D     J
+          / \   / \
+         B   E G   K
+        / \     \
+       A   C     I
+
+Preorder Traversal - F D B A C E J G I K
+*/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+struct Node {
+    char data;
+    Node* left;
+    Node* right;
+};
+
+Node* GetNewNode(char data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* Insert(Node* root, char data) {
+    if(root == NULL)
+        root = GetNewNode(data);
+    else if(data <= root->data)
+        root->left = Insert(root->left,data);
+    else
+        root->right = Insert(root->right,data);
+    return root;
+}
+
+void PreorderTraversal(Node* root) {
+    if(root == NULL) return;
+    printf("%c ",root->data);
+    PreorderTraversal(root->left);
+    PreorderTraversal(root->right);
+}
+
+int main() {
+    Node* root = NULL; //Empty tree
+    int c;
+    do {
+    printf("1. Insert element\n2. Display tree (Preorder) \n");
+    printf("Enter your choice ? \n");
+    int ch;
+    scanf("%d",&ch);
+    switch(ch) {
+    case 1:
+        {
+            char data;
+            printf("Enter the node value ?\n");
+            fflush(stdin);
+            data = getchar();
+            root = Insert(root,data);
+            break;
+        }
+    case 2:
+        {
+            printf("Tree is : ");
+            PreorderTraversal(root);
+            printf("\n");
+            break;
+        }
+    default:
+        printf("Incorrect choice !\n");
+    }
+    printf("Want to continue ? (1. Yes 2. No)\n");
+    scanf("%d",&c);
+    } while(c == 1);
+}
